79a7681f81cf0c35fd12a980d132c5743527ffef
Kateryna Velychko <107401289+Kate2603@users.noreply.github.com>
2025-06-05 18:46:25 +0300

    add changes

---
+++ src/redux/selectors.js
@@ -0,0 +1,22 @@
+import { createSelector } from "@reduxjs/toolkit";
+
+const selectCampers = (state) => state.campers.list;
+const selectFilters = (state) => state.filters;
+
+export const selectFilteredCampers = createSelector(
+  [selectCampers, selectFilters],
+  (campers, filters) => {
+    return campers.filter((camper) => {
+      if (
+        filters.search &&
+        !camper.name.toLowerCase().includes(filters.search.toLowerCase())
+      ) {
+        return false;
+      }
+      if (filters.location && camper.location !== filters.location) {
+        return false;
+      }
+      return true;
+    });
+  },
+);
