6d046a69328c96ec4a6da814f864de9f1078296d
Kateryna Velychko <107401289+Kate2603@users.noreply.github.com>
2025-06-10 02:20:19 +0300

    add changes

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/", // Вказуємо базовий шлях до кореня (для Vercel і локальної розробки)
-  build: {
-    outDir: "dist", // Папка для збірки (за замовчуванням dist, можна змінити)
-    sourcemap: true, // Для зручності дебагу (можна вимкнути в продакшені)
-  },
-  server: {
-    open: true, // Автоматично відкриває браузер при запуску dev-сервера
-  },
+  base: "./",
 });
